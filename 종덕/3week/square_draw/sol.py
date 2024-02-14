29ef6f8bc71645ac2a397ac0a070f6417a28e792
김종덕 <kjd742388@gmail.com>
2024-02-14 22:05:43 +0900

    사각형그리기 수정 김종덕 20240214

--- 종덕/3week/square_draw/sol.py
+++ 종덕/3week/square_draw/sol.py
@@ -17,10 +17,12 @@ for tc in range(1, T+1):
             # 이에 따른 알맞은 범위의 우측 하단 좌표를 순회하면서
             for i2 in range(i1, N):
                 for j2 in range(j1, N):
-                    # 조건에 맞는 직사각형이 나올 경우
-                    if field[i1][j1] == field[i2][j2]:
-                        # 그 넓이를 리스트에 넣는다
-                        area.append((i2-i1+1)*(j2-j1+1))
+                    # 자기 자신과 같은 좌표일 경우 제외
+                    if i1 != i2 or j1 != j2:                        
+                        # 조건에 맞는 직사각형이 나올 경우
+                        if field[i1][j1] == field[i2][j2]:
+                            # 그 넓이를 리스트에 넣는다
+                            area.append((i2-i1+1)*(j2-j1+1))
     # 직사각형이 하나도 나오지 않았다면
     if area == []:
         # 테케와 0을 출력
